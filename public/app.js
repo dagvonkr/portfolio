b3e1e0357e6b431d12f72fa10741a0fb0beac95d
Dag von Krogh Bunkholt <dag@forlagetpropell.no>
2015-07-22 20:38:32 +0200

    fonts, sections, some ScrollWatch

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('portfolio', ['ui.router'])
+var app = angular.module('portfolio', ['ui.router', 'ui.bootstrap'])
 .config(function($stateProvider, $urlRouteProvider)
 
 	$urlRouterProvider.otherwise('/')
